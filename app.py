de5c73899addfa22b738b9c7dd6f9d5e01a9ac4b
employeeterminator-collab <employeeterminator@gmail.com>
2026-09-18 10:02:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ try:
                 "🎉 Registration Successful! Voucher has been locked."
             )
             st.rerun()
-
+        st.markdown("---")
   else:
     # Post-verification screen layout
     st.markdown("<br><br>", unsafe_allow_html=True)
