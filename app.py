2990572c13b18390a8c591e955b6bc040f2d5e1d
employeeterminator-collab <employeeterminator@gmail.com>
2026-09-18 09:29:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ hide_streamlit_style = """
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+col, col2, col3 =st.columns([1,2,1])
+with col2:
 st.title("Shisa Kanko-Shi\u2122")
 st.subheader("(Certified Pointing-and-Calling Specialist)") 
 st.title("Examination Registration Portal")
